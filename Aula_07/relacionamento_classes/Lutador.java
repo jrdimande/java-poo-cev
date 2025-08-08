71475fdb39e334cf828f62b46e0d8d5366f7fcaa
jrdimande <timoteojrdimande@gmail.com>
2025-08-08 21:50:00 +0000

    feat(Lutador): Create getters and setters

--- Aula_07/relacionamento_classes/Lutador.java
+++ Aula_07/relacionamento_classes/Lutador.java
@@ -4,17 +4,102 @@ public class Lutador {
     private String nome;
     private String nacionalidade;
     private int idade;
-    private double altura;
-    private double peso;
+    private float altura;
+    private float peso;
     private String categoria;
     private int vitorias;
     private int derrotas;
     private int empates;
 
     // Metodos especiais
-    public Lutador(String nome, String nacionalidade, int idade, double altura, double peso, int vitorias, int derrotas, int empates){
-
+    public Lutador(String nome, String nacionalidade, int idade, float altura, float peso, int vitorias, int derrotas, int empates){
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+        this.vitorias = vitorias;
+        this.empates = empates;
 
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getVitorias() {
+        return vitorias;
+    }
+
+    public void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+    public void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+
+    public int getEmpates() {
+        return empates;
+    }
+
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
+
+    // Metodos publicos
+    public void apresentar(){}
+    public void status(){}
+    public void ganharLuta(){}
+    public void perderLuta(){}
+    public void empatarLuta(){}
 
 }
