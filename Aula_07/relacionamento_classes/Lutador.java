0d6dc3580b496caf6d27b97553eeb961ec748b04
jrdimande <timoteojrdimande@gmail.com>
2025-08-08 21:34:45 +0000

    ...

---
+++ Aula_07/relacionamento_classes/Lutador.java
@@ -0,0 +1,20 @@
+package Aula_07.relacionamento_classes;
+
+public class Lutador {
+    private String nome;
+    private String nacionalidade;
+    private int idade;
+    private double altura;
+    private double peso;
+    private String categoria;
+    private int vitorias;
+    private int derrotas;
+    private int empates;
+
+    // Metodos especiais
+    public Lutador(String nome, String nacionalidade, int idade, double altura, double peso, int vitorias, int derrotas, int empates){
+
+
+    }
+
+}
